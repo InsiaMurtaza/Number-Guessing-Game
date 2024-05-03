21b1c33bda7067386ee300d4edfa58a8130bdbf0
InsiaMurtaza <165552743+InsiaMurtaza@users.noreply.github.com>
2024-05-03 11:33:27 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+const secretNumber = Math.floor(Math.random() * 8 + 1);
+const answer = await inquirer.prompt([{
+        name: "userInput",
+        message: "Guess any Number between 1 and 8: ",
+        type: "number",
+        transformer: (input) => {
+            return chalk.blue.bold(input);
+        },
+    }
+]);
+if (answer.userInput === secretNumber) {
+    console.log(chalk.yellow("Congratulations!!! You guessed the RIGHT NUMBER."));
+}
+else {
+    console.log(chalk.yellow("Oops!!You guessed a WRONG NUMBER!!! The CORRECT NUMBER was " + (secretNumber)));
+}
